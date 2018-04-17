39be0471de3d55d8197c03eab80e54dd400d0cee
tskdsb <tsk912@qq.com>
2018-04-17 21:52:49 +0800

    【新增功能】加上go标准库实现的FileServer，作为备用下载

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
   flag.Parse()
 
   http.HandleFunc("/", fileServer.ShowRouterS)
+  http.Handle("/SimpleDownload/", http.StripPrefix("/SimpleDownload/", http.FileServer(http.Dir(fileServer.BASE_PATH))))
 
   http.HandleFunc("/download", fileServer.Download)
   http.HandleFunc("/upload", fileServer.Upload)
